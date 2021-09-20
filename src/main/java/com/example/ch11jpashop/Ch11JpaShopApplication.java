c18c242d8e2389eb92f8cc28ca25a3a6459eb9dc
songseongbin <ppsosqq@gmail.com>
2021-09-20 11:29:56 +0900

    JPA 프로그래밍 - ch11. 웹 애플리케이션 제작

---
+++ src/main/java/com/example/ch11jpashop/Ch11JpaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch11jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch11JpaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch11JpaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ch11jpashop/Ch11JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch11jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch11JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
